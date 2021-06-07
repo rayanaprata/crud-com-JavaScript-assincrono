38ee0cc60aa7507e88526fa513ce047e52827ce0
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-06-06 23:03:16 -0300

    link loupe

--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -12,6 +12,8 @@ export const clienteService = {
 // mockagem dos dados para fazer simulações
 // npx json-server --watch db.json
 
+// http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
+
 // antigo listaClientes () => {
 //   const promise = new Promise((resolve, reject) => {
 //     //O objeto XMLHttpRequest fornece os métodos para realizar a comunicação
